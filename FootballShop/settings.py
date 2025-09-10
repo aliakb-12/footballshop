d11128e6ddf61e51e6b6808e36fd66c93a408917
aliakb-12 <alimurtadha2006@gmail.com>
2025-09-10 10:27:44 +0700

    Fixing dissalowed host bug

--- FootballShop/settings.py
+++ FootballShop/settings.py
@@ -143,5 +143,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ali-akbar45-footballshop.pbp.cs.ui.ac.id"]
 
--- main/apps.py
+++ main/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class FootballappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+    
+    ## Fixing dissalowed host bug
+    a = 12 
